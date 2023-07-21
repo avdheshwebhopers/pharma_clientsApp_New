dc19c661a1f9061fd9455b22aa19a7920acec629
webhopersmac <avdesh.webhopers@gmail.com>
2023-07-21 17:59:58 +0530

    initial Commit

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors{
 
-  static Color primaryColor = const Color(0xFF00125C);
+  static Color primaryColor = const Color(0xFF931A0C);
   //static Color primaryColor = Color(0xFF760101);
   static Color backgroundColor = const Color(0xFFF6F9FE);
-  static Color barColor = const Color(0xFFE6F2FF);
+  static Color barColor = const Color(0xFFFFE6EC);
   static Color borderColor = const Color(0xFFD0D4D8);
   static Color iconColor = const Color(0xFF1590D6);
   static Color mrcontaonerheading = Colors.black;
